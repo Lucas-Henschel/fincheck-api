d7bddb4140e470b2a5cd7d9c032efbbdd1460567
Lucas Henschel <lucas101henschel@gmail.com>
2023-11-07 12:58:50 -0300

    Reorganized authetication routes

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthenticateDto } from './dto/authenticate.dto';
+import { SignineDto } from './dto/signin';
+import { SignupDto } from './dto/signup';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signin')
-  autheticate(@Body() authenticateDto: AuthenticateDto) {
-    return this.authService.autheticate(authenticateDto);
+  signin(@Body() signineDto: SignineDto) {
+    return this.authService.signin(signineDto);
+  }
+
+  @Post('signup')
+  create(@Body() signupDto: SignupDto) {
+    return this.authService.signup(signupDto);
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,15 +1,16 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { AuthenticateDto } from './dto/authenticate.dto';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { SignineDto } from './dto/signin';
 import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
-import { compare } from 'bcryptjs';
+import { compare, hash } from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
+import { SignupDto } from './dto/signup';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly usersRepository: UsersRepository, private readonly jwtService: JwtService) {}
 
-  async autheticate(authenticateDto: AuthenticateDto) {
-    const { email, password } = authenticateDto;
+  async signin(signinDto: SignineDto) {
+    const { email, password } = signinDto;
 
     const user = await this.usersRepository.findUnique({
       where: { email },
@@ -25,8 +26,59 @@ export class AuthService {
       throw new UnauthorizedException('Invalid crendetials');
     } 
 
-    const accessToken = await this.jwtService.signAsync({ sub: user.id });
+    const accessToken = await this.generateAccessToken(user.id);
 
     return { accessToken }
+  }
+
+  async signup(signupDto: SignupDto) {
+    const { name, email, password } = signupDto;
+
+    const emailTaken = await this.usersRepository.findUnique({
+      where: { email },
+      select: { id: true },
+    })
+
+    if (emailTaken) {
+      throw new ConflictException('This email is already in use.');
+    }
+
+    const hashedPassword = await hash(password, 12);
+
+    const user = await this.usersRepository.create({
+      data: { 
+        name, 
+        email, 
+        password: hashedPassword, 
+        categories: {
+          createMany: {
+            data: [
+              // Income
+              { name: 'Salário', icon: 'salary', type: 'INCOME' },
+              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
+              { name: 'Outro', icon: 'other', type: 'INCOME' },
+              // Expense
+              { name: 'Casa', icon: 'home', type: 'EXPENSE' },
+              { name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
+              { name: 'Educação', icon: 'education', type: 'EXPENSE' },
+              { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
+              { name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
+              { name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
+              { name: 'Transporte', icon: 'transport', type: 'EXPENSE' },
+              { name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
+              { name: 'Outro', icon: 'other', type: 'EXPENSE' },
+            ]
+          }
+        } 
+      }
+    });
+ 
+    const accessToken = await this.generateAccessToken(user.id);
+
+    return { accessToken };
+  }
+
+  private async generateAccessToken(userId: string) {
+    return await this.jwtService.signAsync({ sub: userId });
   }
 }
